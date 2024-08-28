9708098222acd279263c9eae5bf949a51326859f
Kalyan Pidugu <pidugukalyan@gmail.com>
2024-08-28 13:53:07 +0530

    Created UI

--- ViewDesign/app/src/main/java/com/kp/viewdesign/LaunchActivity.kt
+++ ViewDesign/app/src/main/java/com/kp/viewdesign/LaunchActivity.kt
@@ -1,11 +1,22 @@
 package com.kp.viewdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+        var registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        var loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
     }
 }
--- ViewDesign/app/src/main/java/com/kp/viewdesign/MainActivity.kt
+++ ViewDesign/app/src/main/java/com/kp/viewdesign/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.kp.viewdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var layout = findViewById<LinearLayout>(R.id.firstView)
+        layout.setOnClickListener {
+            startActivity(Intent(this, LaunchActivity::class.java))
+        }
     }
 }
